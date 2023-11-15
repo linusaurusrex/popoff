d39da109fdd2f3e607b7933c86266803f55757d1
Linus Brogan <git@linusbrogan.com>
2023-11-14 20:24:31 -0800

    Prevent extraneous link actions

--- docs/demo/popup.js
+++ docs/demo/popup.js
@@ -1,4 +1,5 @@
-function popup() {
+function popup(event) {
+	event.preventDefault();
 	window.open('popup.html', '_blank', 'popup,width=200,height=200,left=100,top=100');
 }
 document.querySelector('#popup-link').addEventListener('click', popup);
