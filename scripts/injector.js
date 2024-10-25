47552f81f57cfd7c23d1d0f24b6da4106230d039
Linus Brogan <git@linusbrogan.com>
2024-10-24 17:28:49 -0700

    Run open script directly
    Run the script in the "MAIN" ExecutionWorld instead of injecting as a script tag to avoid CSP errors.

--- scripts/injector.js
+++
@@ -1,5 +0,0 @@
-(() => {
-	const script = document.createElement('script');
-	script.src = chrome.runtime.getURL('scripts/open.js');
-	(document.head || document.documentElement).appendChild(script);
-})();
